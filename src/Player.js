59d2cce259436c88417e2cdc2220a8852c3a3ee8
lvaccari12 <70210826+lvaccari12@users.noreply.github.com>
2023-04-12 11:52:42 +0100

    first commit

---
+++ src/Player.js
@@ -0,0 +1,22 @@
+class Player {
+    constructor(x, y, size) {
+        this.x = x;
+        this.y = y;
+        this.size = size;
+
+    }
+
+    move(dx, dy) {
+        this.x += dx;
+        this.y += dy;
+    }
+
+    draw(context) {
+        context.fillStyle = '#f00';
+        context.textBaseLine = 'hanging';
+        context.font = '16px Helvetica';
+        context.fillText('@', this.x * this.size, this.y * this.size);
+    }
+}
+
+export default Player;
